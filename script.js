f977bf87b8659d6494425f89eabf5aa77080239a
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-24 14:59:53 -0400

    added style

---
+++ script.js
@@ -0,0 +1,19 @@
+function repeat() {
+    document.querySelector('#par').textContent = "";
+    let text = "I am a web developer...";
+    let i = 0;
+    let speed = 100;
+
+    function type() {
+        if (i < text.length) {
+            document.querySelector('#par').textContent += text.charAt(i);
+            i++;
+            setTimeout(type, speed)
+
+        }
+    }
+
+    type()
+}
+
+setInterval(repeat, 3000)
